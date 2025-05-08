28539281397fa641ad0b99fc3af29109147dae2f
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-05-08 15:22:54 +0100

    Register new users

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -82,8 +82,7 @@ namespace SuperShop.Controllers
                 var product = _converterHelper.ToProduct(model, imageId, true);
 
 
-                // TODO: Modificar para o user que tiver logado
-                product.User = await _userHelper.GetUserByEmailAsync("brcrodrigues96@gmail.com");
+                product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 await _productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -131,8 +130,7 @@ namespace SuperShop.Controllers
 
                     var product = _converterHelper.ToProduct(model, imageId, false);
 
-                    // TODO: Modificar para o user que tiver logado
-                    product.User = await _userHelper.GetUserByEmailAsync("brcrodrigues96@gmail.com");
+                    product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                     await _productRepository.UpdateAsync(product);
 
                 }
@@ -153,6 +151,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
