08bf0785b607adb91e30a5ea982ffda75779d957
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-05-14 18:27:27 +0100

    Increase, decrease and delete detail temp item

--- SuperShop/Controllers/OrdersController.cs
+++ SuperShop/Controllers/OrdersController.cs
@@ -52,5 +52,42 @@ namespace SuperShop.Controllers
 
             return View(model);
         }
-    }
+
+        public async Task<IActionResult> DeleteItem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await _orderRepository.DeleteDetailTempsAsync(id.Value);
+            return RedirectToAction("Create");
+
+        }
+
+		public async Task<IActionResult> Increase(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			await _orderRepository.ModifyOrderDetailTempQuantityAsync(id.Value,1);
+			return RedirectToAction("Create");
+
+		}
+
+		public async Task<IActionResult> Decrease(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			await _orderRepository.ModifyOrderDetailTempQuantityAsync(id.Value, -1);
+			return RedirectToAction("Create");
+
+		}
+
+	}
 }
